036a5170eb2bbac7a15df6806b1207f5f885537d
canitzp <canitzp@gmail.com>
2021-12-01 17:43:07 +0100

    **38.0.0:** - Update to 1.18 - New versioning numbering, still compatible with semver, but now the major number is equal to the major of the used minecraft forge version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+**38.0.0:**
+- Update to 1.18
+- New versioning numbering, still compatible with semver, but now the major number is equal to the major of the used minecraft forge version
+
 **1.1.0-1.17.1:**
 - Fixed creative items having to large default trace_width
 - Battery level and trace_width now are stored as int and so can reach 2147483647 (2^31-1)
--- src/main/java/de/canitzp/justabattery/JustABattery.java
+++ src/main/java/de/canitzp/justabattery/JustABattery.java
@@ -16,9 +16,9 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
